a9452bc1bbcfb3de9cafb4398f847cebae9b6937
Luciano Ramalho <luciano@ramalho.org>
2011-12-27 10:18:03 -0200

    prototype version of Calendar; tests for weeks starting on Monday

--- calendar.js
+++ calendar.js
@@ -1,25 +1,31 @@
-firstWeekDay = 0;  // 0 = Sunday
+function Calendar(firstWeekDay) {
+    //properties
+    this.firstWeekDay = firstWeekDay || 0; // 0 = Sunday
+};
 
-function weekStartDate(date) {
-    while (date.getDay() !== firstWeekDay) {
-        date.setDate(date.getDate() - 1);
-    }
-    return date;
-}
-
-function monthDatesCalendar(year, month) {
-    var weeks = [];
-    var week = []
-    var date, i;
-    date = weekStartDate(new Date(year, month, 1));
-    do {
-        for (i=0; i<7; i++) {
-            week.push(date);
-            date = new Date(date);
-            date.setDate(date.getDate() + 1);
+Calendar.prototype = {
+    constructor : Calendar,
+    weekStartDate : function weekStartDate(date) {
+        var startDate = new Date(date.getTime());
+        while (startDate.getDay() !== this.firstWeekDay) {
+            startDate.setDate(startDate.getDate() - 1);
         }
-        weeks.push(week);
-        week = [];
-    } while (date.getMonth()<=month);
-    return weeks;
-}
+        return startDate;
+    },
+    monthDatesCalendar : function monthDatesCalendar(year, month) {
+        var weeks = [];
+        var week = [];
+        var date, i;
+        date = this.weekStartDate(new Date(year, month, 1));
+        do {
+            for (i=0; i<7; i++) {
+                week.push(date);
+                date = new Date(date.getTime());
+                date.setDate(date.getDate() + 1);
+            }
+            weeks.push(week);
+            week = [];
+        } while (date.getMonth()<=month);
+        return weeks;
+    }
+};
--- tests/calendar_tests.js
+++ tests/calendar_tests.js
@@ -1,23 +1,48 @@
 var jan_1_2012 = new Date(2012,0,1);
 
 test('January 1, 2012 is Sunday', function() {
+    var calSun = new Calendar(); // calendar with Sunday as first day of the week
     equal(jan_1_2012.toISOString().slice(0,10), '2012-01-01', 'month 0 is January');
     equal(jan_1_2012.getDay(), 0, 'weekday 0 is Sunday');
 });
-test('weekStartDate', function() {
-    equal(weekStartDate(jan_1_2012), jan_1_2012, 'Jan. 1st 2012 is the start of a week');
-    deepEqual(weekStartDate(new Date(2012,0,2)), jan_1_2012, 'first week of 2012 starts on Jan. 1st');
-    deepEqual(weekStartDate(new Date(2012,0,7)), jan_1_2012, 'first week of 2012 starts on Jan. 1st');
-    deepEqual(weekStartDate(new Date(2012,0,8)), new Date(2012,0,8), 'second week of 2012 starts on Jan. 8th');
-    deepEqual(weekStartDate(new Date(2012,0,10)), new Date(2012,0,8), 'second week of 2012 starts on Jan. 8th');
+test('weekStartDate, Sunday', function() {
+    var calSun = new Calendar(); // calendar with Sunday as first day of the week
+    deepEqual(calSun.weekStartDate(jan_1_2012), jan_1_2012, 'Jan. 1st 2012 is the start of a week');
+    deepEqual(calSun.weekStartDate(new Date(2012,0,2)), jan_1_2012, 'first week of 2012 starts on Jan. 1st');
+    deepEqual(calSun.weekStartDate(new Date(2012,0,7)), jan_1_2012, 'first week of 2012 starts on Jan. 1st');
+    deepEqual(calSun.weekStartDate(new Date(2012,0,8)), new Date(2012,0,8), 'second week of 2012 starts on Jan. 8th');
+    deepEqual(calSun.weekStartDate(new Date(2012,0,10)), new Date(2012,0,8), 'second week of 2012 starts on Jan. 8th');
 });
-test('monthDatesCalendar', function() {
-    var mdc_jan_2012 = monthDatesCalendar(2012,0);
-    var mdc_feb_2012 = monthDatesCalendar(2012,1);
+test('weekStartDate, Monday', function() {
+    var calMon = new Calendar(1); // calendar with Monday as first day of the week
+    deepEqual(calMon.weekStartDate(jan_1_2012), new Date(2011,11,26),
+        'December. 26th 2011 is the start of the first week of 2012');
+    deepEqual(calMon.weekStartDate(new Date(2012,0,2)), new Date(2012,0,2),
+        'second week of 2012 starts on Jan. 2nd');
+    deepEqual(calMon.weekStartDate(new Date(2012,0,8)), new Date(2012,0,2),
+        'second week of 2012 starts on Jan. 2nd');
+    deepEqual(calMon.weekStartDate(new Date(2012,0,9)), new Date(2012,0,9),
+        'third week of 2012 starts on Jan. 9th');
+});
+test('monthDatesCalendar, Sunday', function() {
+    var calSun = new Calendar(); // calendar with Sunday as first day of the week
+    var mdc_jan_2012 = calSun.monthDatesCalendar(2012,0);
+    var mdc_feb_2012 = calSun.monthDatesCalendar(2012,1);
     equal(mdc_jan_2012.length, 5, 'January 2012 spans 5 calendar weeks');
     equal(mdc_feb_2012.length, 5, 'February 2012 spans 5 calendar weeks');
     deepEqual(mdc_jan_2012[0][0], jan_1_2012, 'first Sunday is Jan. 1st. = '+mdc_jan_2012[0][0]);
     deepEqual(mdc_jan_2012[1][0], new Date(2012,0,8), 'second Sunday is Jan. 8th.');
     deepEqual(mdc_jan_2012[mdc_jan_2012.length-1][6], new Date(2012,1,4), 'last Saturday Feb. 4th.');
     deepEqual(mdc_feb_2012[mdc_feb_2012.length-1][6], new Date(2012,2,3), 'last Saturday Mar. 3rd. ='+mdc_feb_2012[4][6]);
+});
+test('monthDatesCalendar, Monday', function() {
+    var calMon = new Calendar(1); // calendar with Monday as first day of the week
+    var mdc_jan_2012 = calMon.monthDatesCalendar(2012,0);
+    var mdc_feb_2012 = calMon.monthDatesCalendar(2012,1);
+    equal(mdc_jan_2012.length, 6, 'January 2012 spans 6 calendar weeks');
+    equal(mdc_feb_2012.length, 5, 'February 2012 spans 5 calendar weeks');
+    deepEqual(mdc_jan_2012[0][0], new Date(2011,11,26), 'first Monday is Dec. 26th. = '+mdc_jan_2012[0][0]);
+    deepEqual(mdc_jan_2012[1][0], new Date(2012,0,2), 'second Monday is Jan. 2nd.');
+    deepEqual(mdc_jan_2012[mdc_jan_2012.length-1][6], new Date(2012,1,5), 'last Sunday Feb. 5th.');
+    deepEqual(mdc_feb_2012[mdc_feb_2012.length-1][6], new Date(2012,2,4), 'last Sunday Mar. 4th. ='+mdc_feb_2012[4][6]);
 });
